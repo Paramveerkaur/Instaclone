9de97eed5469ec4f02d5d3b1bdb97902ff31f6b2
Paramveer Kaur <pkaur5496@conestogac.on.ca>
2017-06-20 13:42:38 +0000

    “commit”

--- server.js
+++ server.js
@@ -6,8 +6,8 @@
 var http = require('http');
 var path = require('path');
 var express = require('express');
-var mongoose=require('mongoose');
-var Post=require('./models/Posts.js');
+var mongoose = require('mongoose');
+var Post = require('./models/Posts.js');
 //
 // ## SimpleServer `SimpleServer(obj)`
 //
@@ -19,7 +19,7 @@ var server = http.createServer(router);
 
 mongoose.connect('mongodb://admin:jojo12345@ds127842.mlab.com:27842/asgndb');
 
-var post=new Post({
+/*var post=new Post({
   image:'./img/priyanka-baywatch.jpg',
   comment:'cool baywatch wallpaper',
   likeCount: 0,
@@ -34,48 +34,45 @@ post.save(function(err)
   else
   console.log('posted');
 });
-
+*/
 
 router.use(express.static(path.resolve(__dirname, 'client')));
-router.use(express.bodyParser()); 
-router.get('/',function(req,res)
-{
+router.use(express.bodyParser());
+
+router.get('/', function(req, res) {
   console.log('client request posts.html');
-  res.sendfile(path.join(__dirname,'client/view','posts.html'));
+  res.sendfile(path.join(__dirname, 'client', 'index.html'));
 });
-router.post('/posts',function(req,res)
-{
+
+router.post('/posts', function(req, res) {
   console.log('client request postslist');
-  
+
   Post.find({})
-  .then(function(paths){
-    res.json(paths);
-  })
-});
-
-router.post('/incrLike',function(req,res)
-{
-  console.log('increment like posts' +req.body.id);
-  Post.findById(req.body.id)
-  .then(function(post){
-    post.likeCount++;
-    return post.save(post);
-  })
-  .then(function(post)
-  {
-    res.json({
-      id: req.body.id,count:post.likeCount
+    .then(function(paths) {
+      res.json(paths);
     });
-  })
-  .catch(function(err)
-  {
-    console.log(err);
-  })
+});
+
+router.post('/incrLike', function(req, res) {
+  console.log('increment like posts' + req.body.id);
+  Post.findById(req.body.id)
+    .then(function(post) {
+      post.likeCount++;
+      return post.save(post);
+    })
+    .then(function(post) {
+      res.json({
+        id: req.body.id,
+        count: post.likeCount
+      });
+    })
+    .catch(function(err) {
+      console.log(err);
+    })
 });
 
 
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
   var addr = server.address();
   console.log("Chat server listening at", addr.address + ":" + addr.port);
 });
-
